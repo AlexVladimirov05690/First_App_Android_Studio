c02efcfe28d770a7b222919b30d96cb6f7feeccc
AlexVladimirov05690 <93385788+AlexVladimirov05690@users.noreply.github.com>
2022-03-27 18:58:19 +0300

    Add posters and Toasts

--- app/src/main/java/com/example/findfilms/MainActivity.kt
+++ app/src/main/java/com/example/findfilms/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.findfilms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initButtons()
+    }
+
+    fun initButtons() {
+        find_film.setOnClickListener{
+            Toast.makeText(this, "Найти фильм", Toast.LENGTH_SHORT).show()
+        }
+        favorites.setOnClickListener{
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        recommendations.setOnClickListener{
+            Toast.makeText(this, "Рекомендации", Toast.LENGTH_SHORT).show()
+        }
+        wishlist.setOnClickListener{
+            Toast.makeText(this, "В список желаний", Toast.LENGTH_SHORT).show()
+        }
+        setting.setOnClickListener{
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
